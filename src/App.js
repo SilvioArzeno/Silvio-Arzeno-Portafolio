4e4666f479e757592bd945d11f80253f1d9f3a3a
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-06-01 12:57:00 -0400

    New link to github

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SilvioArzeno"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Click Here for more info
         </a>
       </header>
     </div>
